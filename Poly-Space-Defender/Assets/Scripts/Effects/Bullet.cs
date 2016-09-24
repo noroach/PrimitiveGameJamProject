42afb4205a128de5d0f55caa158410a5b07d3009
noroach <noroach@indiana.edu>
2016-09-24 15:52:01 -0400

    Started the shooting mechanics

---
+++ Poly-Space-Defender/Assets/Scripts/Effects/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public Rigidbody bullet;
+	public float velocity;
+
+	public Material PlayerBulletMat;
+	public Material EnemyBulletMat;
+
+
+	// Use this for initialization
+	void Start () {
+		bullet = gameObject.GetComponent<Rigidbody> ();
+		Shoot ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void Shoot () {
+		bullet.AddForce (transform.up * velocity, ForceMode.VelocityChange);
+	}
+}
---
+++ Poly-Space-Defender/Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour {
+
+	//gun stats, will take these from the global player settings
+	public float MainGunDamage;
+	public float MainGunRateOfFire;
+	public GameObject MainBulletPrefab;
+
+	// we also need all the gun positions
+	public GameObject MainGunLeft;
+	public GameObject MainGunRight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")){
+			MainFire ();
+		}
+	}
+
+	void MainFire(){
+		// when we instantiate we also need to set which layer the bullet is on
+		// because by default - they are enemy bullets
+		Vector3 LeftPos = new Vector3(transform.position.x,  transform.position.y, transform.position.z);
+		Quaternion bulletRot = Quaternion.Euler(transform.rotation.x - 90, transform.rotation.y, transform.rotation.z);
+		Instantiate(MainBulletPrefab, MainGunLeft.transform.position, bulletRot);  //left bullet
+		Instantiate(MainBulletPrefab, MainGunRight.transform.position, bulletRot); //right bullet
+	}
+
+
+
+}
